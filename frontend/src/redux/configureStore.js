6c44be4ca50ac215cfe5f24191c3915eb686183f
YunSoo Kang <yskang@YunSooui-MacBook-Pro.local>
2018-05-06 01:04:38 +0900

    ~ #3-16

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,21 +1,45 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import users from "redux/module/users";
+import { routerReducer, routerMiddleware } from "react-router-redux";
+import createHistory from "history/createBrowserHistory";
+import { composeWithDevTools } from "redux-devtools-extension";
+// import Reactotron from "ReactotronConfig";
+import { i18nState } from "redux-i18n";
+
+
 
 const env = process.env.NODE_ENV;
 
-const middlewares = [thunk];
+const history = createHistory();
 
+const middlewares = [thunk, routerMiddleware(history)];
+
+// if (env === "development") {
+//   const { logger } = require("redux-logger");
+//   middlewares.push(logger);
+// }
+
+const reducer = combineReducers({
+  users,
+  routing: routerReducer,
+  i18nState
+
+});
+
+let store;
 if (env === "development") {
   const { logger } = require("redux-logger");
   middlewares.push(logger);
+  store = initialState =>
+      // Reactotron.createStore(reducer, applyMiddleware(...middlewares));
+      createStore(
+          reducer,
+          composeWithDevTools(applyMiddleware(...middlewares)));
+} else {
+  store = initialState => createStore(reducer, applyMiddleware(...middlewares));
 }
 
-const reducer = combineReducers({
-  users
-});
-
-let store = initialState =>
-    createStore(reducer, applyMiddleware(...middlewares));
+export { history };
 
 export default store();
